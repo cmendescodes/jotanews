5b8cfe4c79770a5618c21ddc839460c4492de105
Caio <boatblackswan@gmail.com>
2025-07-11 02:44:00 -0300

    Projeto JotaNews v2

--- jota_news/settings.py
+++ jota_news/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'tramway.proxy.rlwy.net'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'PORT': os.environ.get('POSTGRES_PORT', '47183'),
     }
 }
 
